55eaa6518888eebb34f3e001312ea090d555a827
pyrocreators <igoreznichenko@gmail.com>
2025-05-31 20:26:27 +0200

    removed pront

--- agents/tools.py
+++ agents/tools.py
@@ -16,7 +16,6 @@ def embed_and_store_single_input(text: str) -> str:
        """
     try:
         chunks = chunk_text(text)
-        print(chunks)
         get_vector_store(chunks, HARDCODED_STORE_PATH)
         return f"Successfully embedded and stored vectors at {HARDCODED_STORE_PATH}."
     except Exception as e:
@@ -28,7 +27,6 @@ def qa_from_store(question: str) -> str:
        Retrieves chunks relevant to a question from a stored vector database and returns them.
        """
     try:
-        print(question, 'question')
         vectordb = load_vector_store(HARDCODED_STORE_PATH)
         results = get_top_chunks(question, vectordb)
         return "\n".join([doc.page_content for doc in results])
